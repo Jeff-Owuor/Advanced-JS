dbf9c4f95ce22323b670dbd493f7b8aee2eee7e8
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-11-29 16:51:34 +0300

    Catch method

--- promise.js
+++ promise.js
@@ -5,7 +5,7 @@ let promiseObj = new Promise((resolve,reject) => {
     console.log(`Getting data from database`)
     setTimeout(()=>{
         reject("An error occurred!")
-    },2000)
+    },3000)
 })
 promiseObj.then((val)=>{
     console.log(`Hi! my name is, ${val}`)
@@ -13,3 +13,13 @@ promiseObj.then((val)=>{
     console.log(`${err} Please Try again.`)
 })
 console.log(promiseObj)
+
+//catch method
+let anotherPromise = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+       reject(new Error(`An error occurred!!`))
+    },2000)
+})
+anotherPromise.catch(err=>{
+    console.log(`${err}`)
+})
